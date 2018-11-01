1dec23794fe9d3d1eb8b8b992080962b4fe0403a
Jaeho Cho <jaeho8032@gmail.com>
2018-11-01 15:53:35 +0900

    [#2] views 뼈대 구현
    - views 기본적으로 사용될 함수들 뼈대 작성
    
    Resolves #2

--- wintercoding/todo_list/views.py
+++ wintercoding/todo_list/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import TODO
 
-# Create your views here.
+
+def todo_list(request):
+    pass
+
+
+def todo_add(request):
+    pass
+
+
+def todo_complete_list(request):
+    pass
+
+
+def todo_reorder(request):
+    pass
+
+
+def todo_detail(request):
+    pass
+
+
+def todo_modify(request):
+    pass
+
+
+def todo_delete(request):
+    pass
+
+
+def todo_complete(request):
+    pass
