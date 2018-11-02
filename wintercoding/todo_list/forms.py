9019cc46b9bc6e6e9a3acabe0c7d83d37ee59e58
Jaeho Cho <jaeho8032@gmail.com>
2018-11-02 13:35:19 +0900

    [#3] todo_list 테스트 코드 추가 및 travis CI 설정 파일 추가
    - 테스트 위한 pytest, model-mommy 패키지 요구사항에 추가
    
    - 테스트를 위해 forms 코드 수정 due_date 확인하는 코드 추가
    
    - tests 코드 작성, pytest.ini 작성
    
    - travis.yml 설정 파일 작성
    
    Resolves #3

--- wintercoding/todo_list/forms.py
+++ wintercoding/todo_list/forms.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django import forms
+from django.core.exceptions import ValidationError
 from . import models
 
 class TODOForm(forms.ModelForm):
@@ -10,6 +13,15 @@ class TODOForm(forms.ModelForm):
             }
         fields = ['title', 'content', 'due_date', 'due_time']
 
+    def clean_due_date(self):
+        date = self.cleaned_data['due_date']
+
+        # 마감 기한이 미래로 설정되어 있는지 확인
+        if date < datetime.date.today():
+            raise ValidationError('잘못된 날짜 - 현재 시간보다 미래를 설정해야 함')
+
+        return date
+
 class TODOModifyForm(forms.ModelForm):
     class Meta:
         model = models.TODO
@@ -18,3 +30,12 @@ class TODOModifyForm(forms.ModelForm):
             'due_time': forms.TimeInput(attrs={'class': 'timepicki'}),
             }
         fields = ['priority', 'title', 'content', 'due_date', 'due_time', 'status']
+
+    def clean_due_date(self):
+        date = self.cleaned_data['due_date']
+
+        # 마감 기한이 미래로 설정되어 있는지 확인
+        if date < datetime.date.today():
+            raise ValidationError('잘못된 날짜 - 현재 시간보다 미래를 설정해야 함')
+
+        return date
--- wintercoding/todo_list/tests.py
+++ wintercoding/todo_list/tests.py
@@ -1,3 +1,63 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.utils import timezone
+
+from model_mommy import mommy
+
+from .models import TODO
+from .forms import TODOForm, TODOModifyForm
+
+class TestModelsTest(TestCase):
+    
+    def setUp(self):
+        self.author = mommy.make(User)
+        self.todo = mommy.make(
+                TODO, 
+                author=self.author
+                )
+
+    def test_whatever_creation(self):
+        todo = self.todo
+        self.assertIn(todo.status, ('i', 'c', 'e'))
+
+class FormsTest(TestCase):
+    def setUp(self):
+        self.author = mommy.make(User)
+        self.todo = mommy.make(
+            TODO,
+            author=self.author
+        )
+
+    def test_todo_form_due_date_in_past(self):
+        date = datetime.date.today() - datetime.timedelta(days=1)
+        form = TODOForm(data={
+            'title': self.todo.title,
+            'due_date': date,
+            })
+        self.assertFalse(form.is_valid())
+    
+    def test_todo_form_due_date_today(self):
+        date = datetime.date.today()
+        form = TODOForm(data={
+            'title': self.todo.title,
+            'due_date': date,
+            })
+        self.assertTrue(form.is_valid())
+
+    def test_todo_modify_form_due_date_in_past(self):
+        date = datetime.date.today() - datetime.timedelta(days=1)
+        form = TODOForm(data={
+            'title': self.todo.title,
+            'due_date': date,
+            })
+        self.assertFalse(form.is_valid())
+    
+    def test_todo_modify_form_due_date_today(self):
+        date = datetime.date.today()
+        form = TODOForm(data={
+            'title': self.todo.title,
+            'due_date': date,
+            })
+        self.assertTrue(form.is_valid())
