c4f104517785ef1a860f5b6707a542af10d1945c
Jaeho Cho <jaeho8032@gmail.com>
2018-11-04 11:46:00 +0900

    [#7] project directory 한 단계 줄임
    - wsgi 서버 실행 문제로, project directory 한 단계 줄임
    
    Resolves #7

---
+++ wintercoding/settings/prod.py
@@ -0,0 +1,11 @@
+from .common import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+    },
+}
